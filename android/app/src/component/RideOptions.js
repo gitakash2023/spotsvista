38dd6c42eb32f1258ca5404c9657e7583dd7f1a0
Akash <yogibabu32@gmail.com>
2024-01-16 10:02:34 +0530

    .

--- android/app/src/component/RideOptions.js
+++ android/app/src/component/RideOptions.js
@@ -4,14 +4,14 @@ import { View, Text, TouchableOpacity, StyleSheet, Image, ScrollView } from 'rea
 
 const RideOptions = ({ onSelectRide }) => {
   const rideOptions = [
-    { id: 1, title: 'Option 1', price: '$20', driver: 'John Doe', mobile: '123-456-7890', image: require('../Image/car.jpg') },
-    { id: 2, title: 'Option 2', price: '$25', driver: 'Jane Smith', mobile: '987-654-3210', image: require('../Image/car1.jpg') },
-    { id: 3, title: 'Option 3', price: '$18', driver: 'Bob Johnson', mobile: '111-222-3333', image: require('../Image/car3.jpg') },
-    { id: 4, title: 'Option 4', price: '$22', driver: 'Alice Brown', mobile: '444-555-6666', image: require('../Image/car3d.jpg') },
-    { id: 5, title: 'Option 1', price: '$20', driver: 'John Doe', mobile: '123-456-7890', image: require('../Image/car.jpg') },
-    { id: 6, title: 'Option 2', price: '$25', driver: 'Jane Smith', mobile: '987-654-3210', image: require('../Image/car1.jpg') },
-    { id: 7, title: 'Option 3', price: '$18', driver: 'Bob Johnson', mobile: '111-222-3333', image: require('../Image/car3.jpg') },
-    { id: 8, title: 'Option 4', price: '$22', driver: 'Alice Brown', mobile: '444-555-6666', image: require('../Image/car3d.jpg') },
+    { id: 1, title: 'Option 1', price: 20, driver: 'John Doe', mobile: '123-456-7890', image: require('../Image/car.jpg') },
+    { id: 2, title: 'Option 2', price: 25, driver: 'Jane Smith', mobile: '987-654-3210', image: require('../Image/car1.jpg') },
+    { id: 3, title: 'Option 3', price: 18, driver: 'Bob Johnson', mobile: '111-222-3333', image: require('../Image/car3.jpg') },
+    { id: 4, title: 'Option 4', price: 22, driver: 'Alice Brown', mobile: '444-555-6666', image: require('../Image/car3d.jpg') },
+    { id: 5, title: 'Option 1', price: 20, driver: 'John Doe', mobile: '123-456-7890', image: require('../Image/car.jpg') },
+    { id: 6, title: 'Option 2', price: 25, driver: 'Jane Smith', mobile: '987-654-3210', image: require('../Image/car1.jpg') },
+    { id: 7, title: 'Option 3', price: 18, driver: 'Bob Johnson', mobile: '111-222-3333', image: require('../Image/car3.jpg') },
+    { id: 8, title: 'Option 4', price: 22, driver: 'Alice Brown', mobile: '444-555-6666', image: require('../Image/car3d.jpg') },
   ];
 
   return (
